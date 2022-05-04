d3e6a30e5df065095303b03f45fb4d1a8594127c
Simon Taylor <simon@zappar.com>
2022-05-04 12:02:53 +0100

    Switch to the zappar-threejs beta
    - npm install --save @zappar/zappar-threejs@beta
    - Removed the explicit file-loader rule from the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -29,11 +29,6 @@ module.exports = {
 				test: /\.css$/i,
 				use: ['style-loader', 'css-loader'],
 			},
-			{
-				test: /zcv\.wasm$/,
-				type: "javascript/auto",
-				loader: "file-loader"
-			},
 		]
 	}
 };
